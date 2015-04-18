0acf763c0b0f5b7f218075dc38a073f0a440373f
Michael <x>
2015-04-17 20:02:24 -0400

    Added user registration.

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -7,4 +7,8 @@ urlpatterns = patterns('',
         url(r'^add_category/$', views.add_category, name='add_category'),
         url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
         url(r'^page/(?P<page_title_slug>[\w\-]+)/$', views.page, name='page'),
+        
+        url(r'^category/(?P<category_name_slug>\w+)/add_page/$', views.add_page, name='add_page'), # Page for this not done yet.
+        
+        url(r'^register/$', views.register, name='register'),
         )
